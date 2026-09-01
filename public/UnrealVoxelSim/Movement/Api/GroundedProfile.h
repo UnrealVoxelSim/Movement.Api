0102b69b4ccc05156d9c811fe0eb2fc026c2ddfe
Dmitriy Monakhov <dimamon6032403@gmail.com>
2026-09-01 19:35:03 +0200

    Swimming implemented

--- public/UnrealVoxelSim/Movement/Api/GroundedProfile.h
+++ public/UnrealVoxelSim/Movement/Api/GroundedProfile.h
@@ -18,16 +18,18 @@ namespace UnrealVoxelSim::Movement::Api
 		Math::Api::FixedPointScalar MaximumSpeed{Math::Api::FixedPointScalar::FromWhole(4)};
 		Math::Api::FixedPointScalar JumpSpeed{Math::Api::FixedPointScalar::FromWhole(7)};
 		Math::Api::FixedPointScalar Gravity{Math::Api::FixedPointScalar::FromWhole(-20)};
+		Math::Api::FixedPointScalar SwimmingSpeed{};
 		Math::Api::FixedPointScalar CollisionSkin{
-			Math::Api::FixedPointScalar::FromRaw(Math::Api::FixedPointScalar::OneRaw / 16)
-		};
+			Math::Api::FixedPointScalar::FromRaw(Math::Api::FixedPointScalar::OneRaw / 16)};
 
 		[[nodiscard]] constexpr bool IsValid() const noexcept
 		{
 			return Id.IsValid() && Width != 0 && Length != 0 && Height != 0 && MaximumSpeed.Raw() > 0 &&
-				JumpSpeed.Raw() > 0 && Gravity.Raw() < 0 && CollisionSkin.Raw() >= 0 &&
+				JumpSpeed.Raw() > 0 && Gravity.Raw() < 0 && SwimmingSpeed.Raw() >= 0 && CollisionSkin.Raw() >= 0 &&
 				CollisionSkin.Raw() < static_cast<std::int64_t>(Width) * Math::Api::FixedPointScalar::OneRaw / 2 &&
 				CollisionSkin.Raw() < static_cast<std::int64_t>(Length) * Math::Api::FixedPointScalar::OneRaw / 2;
 		}
+
+		[[nodiscard]] constexpr bool CanSwim() const noexcept { return SwimmingSpeed.Raw() > 0; }
 	};
 }
